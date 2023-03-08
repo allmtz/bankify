c175b10b4cfc70617952dd3074be2e33e03c323d
Allan Martinez <martinezallan123@gmail.com>
2023-03-08 13:46:31 -0800

    server WIP

---
+++ server/index.js
@@ -0,0 +1,58 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+const tempId = 1;
+
+let tempUser = {
+  name: "John",
+  balance: 500,
+};
+
+app.listen(PORT, () => {
+  console.log(`server live on port http://localhost:${PORT}`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
+
+app.get(`/${tempId}/balance`, (req, res) => {
+  res.send(String(tempUser.balance));
+});
+
+app.get(`/${tempId}/name`, (req, res) => {
+  res.send(String(tempUser.name));
+});
+
+app.post(`/${tempId}/balance/deposit`, (req, res) => {
+  const amount = Number(req.body.amount);
+
+  //balance will be user specific
+  //sanitize request
+  if (amount <= 0) {
+    res.status(400).send("can only deposit amounts greater than 0");
+  }
+
+  tempUser.balance += amount;
+
+  res.sendStatus(200);
+});
+
+app.post(`/${tempId}/balance/withdraw`, (req, res) => {
+  const amount = Number(req.body.amount);
+
+  //balance will be user specific
+  //sanitize request
+  if (amount <= 0) {
+    res.status(400).send("can only withdraw amounts greater than 0");
+  }
+  if (amount > tempUser.balance) {
+    res.status(400).send("balance isn't large enough");
+  }
+
+  tempUser.balance -= amount;
+
+  res.sendStatus(200);
+});
