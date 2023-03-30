81c9d845d0fd2efd46d5b5cbf42f956e25fb90db
emauricio100 <emauricio@ucdavis.edu>
2023-03-29 18:18:55 -0700

    GET request for balance added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,22 +1,27 @@
 import "../styles/HomePage.css";
-import { useRef } from "react";
+import { useState } from "react";
 
 export const HomePage = ({ userID }) => {
-  function bal() {
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify(userID),
-    };
+  const [userBalance, setUserBalance] = useState(null);
+  async function bal() {
+    let response;
+    let responseJson;
+    try {
+      response = await fetch(`http://localhost:3000/user/${userID}/balance`);
+      responseJson = await response.json();
+      setUserBalance(responseJson.balance);
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
   }
+
   return (
     <div className="display">
       <div className="container">
         <h2>Balance</h2>
-        <h3></h3>
+        {userBalance !== null && <h4>{userBalance}</h4>}
+        <button onClick={bal}>console</button>
       </div>
     </div>
   );
